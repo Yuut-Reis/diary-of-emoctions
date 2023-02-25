d0881411c8db6061e3c627b28499e0652b8f092d
eletrocp <brernolopes@hotmail.com>
2023-02-24 22:54:22 -0300

    :sparkles: feat: interfaces

--- backend/src/database/interface/userInterface.ts
+++ backend/src/database/interface/userInterface.ts
@@ -1,15 +1,27 @@
 export interface IUser {
   id?: number;
-  username: string;
-  password: string;
-  email: string;
+  username?: string;
+  password?: string;
+  name?: string;
+  role?: string;
 }
 
-export interface ILoginResgister {
+export interface IResgister {
+  username: string,
+  name: string,
+  password: string,
+  pronoun: string,
+}
+
+export interface ILogin {
   username: string,
   password: string,
 }
 
 export interface IToken {
   token: string,
+}
+
+export interface IRole {
+  role: string,
 }
